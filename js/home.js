1e6ff12c5e77de8cff3618b400e46793399df8ff
haruna1256 <haru_kawagishi@icloud.com>
2024-10-30 12:28:29 +0900

    ダイアログ作成

---
+++ js/home.js
@@ -0,0 +1,21 @@
+// ブーストボタン
+const boostBtnBtn = document.getElementById('boostBtn');
+// ダイアログ
+const boostDialog = document.getElementById('boostDialog');
+// 閉じるボタン
+const closeButton = document.getElementById('closeButton');
+
+
+// ブーストボタンを押された時にダイアログを出す
+boostBtn.addEventListener('click', () => {
+    boostDialog.showModal();
+    // モーダルダイアログを表示する際に背景部分がスクロールしないようにする
+    document.documentElement.style.overflow = "hidden";
+});
+
+// モーダルを閉じる
+closeButton?.addEventListener('click', () => {
+    boostDialog.close();
+    // モーダルを解除すると、スクロール可能になる
+    document.documentElement.removeAttribute("style");
+  });
