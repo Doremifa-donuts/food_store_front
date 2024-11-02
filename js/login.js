80c35b370175fb071e007c0daf04fabfb77897c3
haruna1256 <haru_kawagishi@icloud.com>
2024-11-02 18:13:09 +0900

    daialog create

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 // ログインボタン
 const loginBtn = document.getElementById('login-btn');
 
+// ログインダイアログ
+const loginDialog = document.getElementById('login-dialog');
+
 // ログインボタンを押された時にダイアログを出す
 loginBtn.addEventListener('click', () => {
     
