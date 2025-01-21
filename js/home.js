72334871344e08337887688bef962725efb814b5
りんご <hanacoi7243@gmail.com>
2025-01-21 16:42:03 +0900

    混雑状況ボタンのjsできた

--- js/home.js
+++ js/home.js
@@ -19,15 +19,29 @@ switching_toggle.addEventListener('change', () => {
     }
 });
 
-// 混雑状況ボタン、押したら大きくなる処理
-const congestion_situation = document.querySelectorAll('.congestion_situation');
 
-console.log(congestion_situation);
+// 混雑状況ボタン、押したら大きくなる処理
+const congestion_situation = document.querySelectorAll('.congestion_situation'); //混雑状況ボタン
 
 congestion_situation.forEach(button => {
     button.addEventListener("click", (e) => {
-        const id_value = button.id;
-        console.log(id_value);
+        const id_value = button.id; //どの状況が選択されているかID名で取得
+      
+        //現在大きく表示されているものがあれば小さくする
+        if(document.querySelector('.congestion_Big')){
+            big_value = document.querySelector('.congestion_Big'); //現在大きく表示されているもの
+            big_value.classList.remove('congestion_Big');   //大きくするスタイルを外す
+            big_value.classList.add('congestion_small');    //小さいときにつけるstyleをつける
+        }
+
+        //選択されたボタンを大きくする
+        const select_situation = document.getElementById(id_value); //選択されたボタン
+        select_situation.classList.toggle('congestion_Big'); //大きくするスタイルをつける
+
+        //大きく表示されていたら、小さくするスタイルを外す
+        if(select_situation.classList.contains('congestion_Big')){
+            select_situation.classList.remove('congestion_small'); //小さく表示するスタイルを外す
+        }
     });
 });
 
