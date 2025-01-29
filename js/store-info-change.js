5d882d3e48096447195132b4af2d368412eccfe0
Toma <kirima--idea--@au.com>
2025-01-29 12:25:21 +0900

    [add]自身の店舗の詳細を取得する処理を追加 軽微な修正

--- js/store-info-change.js
+++ js/store-info-change.js
@@ -1,8 +1,36 @@
+import { RESTAURANT_URL } from "./variable.js";
 //選択された画像を大きく表示
-const img_file = document.querySelector('#img_file'); //画像選択インプット
-const store_img = document.querySelector('#store_img'); //画像を表示するimgタグ
-console.log(img_file);
+const imgFile = document.querySelector('#img_file'); //画像選択インプット
+const storeImg = document.querySelector('#store_img'); //画像を表示するimgタグ
+//console.log(img_file);
+const jtiToken = localStorage.getItem('JtiToken');
+let restaurantDetail = {};
 
-img_file.addEventListener('change',() => { 
-  store_img.src = img_file.fileUrl;
+fetch(RESTAURANT_URL, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${jtiToken}`
+  },
+  mode: 'cors',
+})
+.then(response => {
+  switch (response.status) {
+    case 200:
+      return response.json();
+    case 401:   //認証情報が正しくなければログイン画面に遷移
+      localStorage.removeItem('JtiToken');
+      window.location.href = './login.html';
+      return;
+    case 404:
+      break;
+  }
+})
+.then(data => {
+  restaurantDetail = data.Response.Data;
+  console.log(restaurantDetail.Address)
+});
+
+imgFile.addEventListener('change',() => {
+  storeImg.src = imgFile.fileUrl;
 });
