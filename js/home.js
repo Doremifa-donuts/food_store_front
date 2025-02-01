0c366525e40e34c3330f2e78cce374fd9ee47acf
りんご <hanacoi7243@gmail.com>
2025-02-01 20:18:51 +0900

    home_reservation style

--- js/home.js
+++ js/home.js
@@ -85,24 +85,24 @@ switchingToggle.addEventListener('change', () => {
         review.style.display= 'block';  //レビューを見えるように
         reservation.style.display = 'none'; //予約表を見えないように
     }
+  
 });
 
 //　レビュー複製処理
 const review_card = document.getElementById('review_card'); //レビューカード（複製されるもの）
-const review_btn = document.getElementById('review_btn'); //仮で作ったカード複製ボタン(動きを確認するための仮置き)
+// const review_btn = document.getElementById('review_btn'); //仮で作ったカード複製ボタン(動きを確認するための仮置き)
 const review_view = document.getElementById('review_view'); //レビューが表示される場所（複製する場所）
 let review_count = 1;   //複製されるid名の変更部分
 
-//レビューカードが複製される処理(動きを確認するための仮置きボタンで)
-review_btn.addEventListener("click",(e) => {
-    const clone_element = review_card.cloneNode(true);  //子要素も含めて複製
-    review_view.appendChild(clone_element);   //現在表示されているものの後に追加
-    review_card.style.display = "flex"; //見えなくしているものを複製しているので見えるように
-    clone_element.id = 'review_card' + review_count;    //複製されたレビューカードのid名変更3x
+// //レビューカードが複製される処理(動きを確認するための仮置きボタンで)
+// review_btn.addEventListener("click",(e) => {
+//     const clone_element = review_card.cloneNode(true);  //子要素も含めて複製
+//     review_view.appendChild(clone_element);   //現在表示されているものの後に追加
+//     review_card.style.display = "flex"; //見えなくしているものを複製しているので見えるように
+//     clone_element.id = 'review_card' + review_count;    //複製されたレビューカードのid名変更3x
 
-    review_count++; //複製されたid名の数字を変更する変更する
-});
-
+//     review_count++; //複製されたid名の数字を変更する変更する
+// });
 
 //お助けブースト周辺人数が変更される処理
 const boost_num = document.getElementById('boost_num');     //周辺人数が表示されている部分
