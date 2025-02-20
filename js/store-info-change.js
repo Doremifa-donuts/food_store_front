bcf1a3872797ce76ea0a645750adefc804415f88
Toma <kirima--idea--@au.com>
2025-02-21 02:19:10 +0900

    [fix]メソッドの使用

--- js/store-info-change.js
+++ js/store-info-change.js
@@ -1,4 +1,5 @@
 import * as url from "./url.js";
+import * as security from "./security.js";
 //選択された画像を大きく表示
 const imgFile = document.querySelector('#img_file'); //画像選択インプット
 const storeImg = document.querySelector('#store_img'); //画像を表示するimgタグ
@@ -21,21 +22,7 @@ fetch(url.RESTAURANT_URL, {
   },
   mode: 'cors',
 })
-.then(response => {
-  switch (response.status) {
-    case 200:
-      return response.json();
-    case 401:   //認証情報が正しくなければログイン画面に遷移
-      localStorage.removeItem('JtiToken');
-      window.location.href = './login.html';
-      return;
-    case 404:
-      break;
-    case 500:
-      localStorage.removeItem('JtiToken');
-      window.location.href = './login.html'
-  }
-})
+.then(response => security.checkStatus(response))
 .then(data => {
   restaurantDetail = data.Response.Data;
   console.log(restaurantDetail);
